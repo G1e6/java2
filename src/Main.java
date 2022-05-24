8e48b05a5d82725c3736c9faa2756dc684100682
G1e6 <gleb-potapov@rambler.ru>
2022-05-24 19:49:24 +0500

    dell

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int stb = 230; //цена билета
+        int bmil = 20; //1 бонусная милля
+        int kolmb = stb / bmil; //количество бонусных миль
+        System.out.println(" Количество бонусных милль = " + kolmb);
+    }
+}
+
