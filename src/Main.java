442e3d9e79d9f4dd7e7881a9e25e11d8a6cdd4e2
G1e6 <gleb-potapov@rambler.ru>
2022-05-26 19:08:19 +0500

    qt

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 230; //цена билета
+        int priceTicket = 230; //цена билета
         int bonusMiles = 20; //1 бонусная милля
-        int amountBonusMiles = ticketPrice / bonusMiles; //количество бонусных миль
+        int amountBonusMiles = priceTicket / bonusMiles; //количество бонусных миль
         System.out.println(" Количество бонусных милль = " + amountBonusMiles);
     }
 }
